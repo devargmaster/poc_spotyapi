15ff92030e88bc9d5db156c4cbea99ed39004b35
WalterArce <walter.arce@davinci.edu.ar>
2024-09-05 00:04:27 -0300

    se agrega las rutas de album para hacer la prueba

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const helloRoute = require('./hello.route');
+const albumRoute = require('./album.route');
 
 function initRoutes(app) {
     app.use('/api/v1/hello', helloRoute);
+    app.use('/api/v1/album', albumRoute);
 }
 
 module.exports = {
